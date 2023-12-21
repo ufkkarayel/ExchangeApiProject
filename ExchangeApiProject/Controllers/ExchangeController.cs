71b8bde6bde5c2618c899d0bd238c7b66308c970
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-21 21:28:26 +0300

    Add project files.

---
+++ ExchangeApiProject/Controllers/ExchangeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace ExchangeApiProject.Controllers
+{
+    public class ExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            #region
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-exchange.p.rapidapi.com/exchange?from=USD&to=TRY&q=1.0"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "ed120ca162msh4a2cf1663625a24p1cae18jsn548260e0d857" },
+        { "X-RapidAPI-Host", "currency-exchange.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+               ViewBag.USDTOTRY=body;
+            }
+            #endregion
+            #region
+            var client2 = new HttpClient();
+            var request2 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-exchange.p.rapidapi.com/exchange?from=EUR&to=TRY&q=1.0"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "ed120ca162msh4a2cf1663625a24p1cae18jsn548260e0d857" },
+        { "X-RapidAPI-Host", "currency-exchange.p.rapidapi.com" },
+    },
+            };
+            using (var response2 = await client2.SendAsync(request2))
+            {
+                response2.EnsureSuccessStatusCode();
+                var body2 = await response2.Content.ReadAsStringAsync();
+                ViewBag.EURTOTRY=body2;
+            }
+            #endregion
+            #region
+            
+            var client3 = new HttpClient();
+            var request3 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-exchange.p.rapidapi.com/exchange?from=GBP&to=TRY&q=1.0"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "ed120ca162msh4a2cf1663625a24p1cae18jsn548260e0d857" },
+        { "X-RapidAPI-Host", "currency-exchange.p.rapidapi.com" },
+    },
+            };
+            using (var response3 = await client3.SendAsync(request3))
+            {
+                response3.EnsureSuccessStatusCode();
+                var body3 = await response3.Content.ReadAsStringAsync();
+                ViewBag.GBPTOTRY=body3;
+                //ViewBag.Rate3=(1/decimal.Parse(body3))*100;
+            }
+            #endregion
+            return View();
+        }
+    }
+}
